1a77c198ac96a80c28b44b6f5a435b563c10d678
delavago1999 <delano_bailey@hotmail.com>
2019-11-15 09:56:05 -0500

    Added comments and readme file

---
+++ README.md
@@ -0,0 +1,27 @@
+# Get Started
+There are a few things you have to get installed bfore you can run this very basic API.
+### Pre API steps
+
+*  make sure you have `node` installed, you can download node [here](https://nodejs.org/en/download/)
+*  also make sure you have `yarn`, you can download yarn [here](https://yarnpkg.com/lang/en/docs/install/#mac-stable) 
+   *  yarn is just like npm so you can use either yarn or npm. npm come with node while yarn has to be installed
+
+## Running the API
+* Open the api folder in VSCODE or any text editor of your choice (not notpad ++ for god sake)
+* in a terminal/cmd/powershell window open the folder directory and run the following command
+  * `yarn install` if your using yarn like me
+  * `npm install` if your using npm which is fine also
+* Now you can run the API with the following command
+  * `yarn start` or `npm start`
+
+# Using the api
+There are a few things we still need to get installed to get to interact with the API
+we need some sort of software for now that we can use to interact with the API routes
+below i listed 2 options:
+
+* Postman can be downloaded from [here](https://www.getpostman.com/)
+* Insomnia can be downloaded from [here](https://insomnia.rest/)
+
+Either of the listed options can be used.
+
+**Navigate to to routes/users.js in the folder for more notes**
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-let messages = [];
+let messages = [];//global array
 
-/* GET users listing. */
+/**
+ * simple route to get the userId the user sent in
+ *  the url
+ * Full URL: http://localhost:3000/users/:userId 
+ * Example: http://localhost:3000/users/100
+ */
 router.get('/:userId', function(req, res, next) {
 
-  let meh = parseInt(req.params.userId);
+  // The userId that is sent in the URL is parsed to an int and stored in the meh variable
+  //the userId is parsed because my default it is a string
+  let meh = parseInt(req.params.userId);//if userId is set to 100 you should get back 100 in your response
 
   res.send({
     success: true,
@@ -16,12 +23,21 @@ router.get('/:userId', function(req, res, next) {
   });
 });
 
+/**
+ * Send messages that will be stored
+ * in an array and returned to the sender
+ * including all the past messages
+ * Full URL: http://localhost:3000/users/message
+ */
 router.post('/message',(req,res,next)=> {
+  //stores incoming message in array
+  //.push() is an array function that is used to put something into an array
   messages.push({
-    message: req.body.message,
-    sender: req.body.sender
+    message: req.body.message,//you can access the message you sent from req.body.message
+    sender: req.body.sender//the sender name you set will be in req.body.sender
   });
 
+  //res.send is used to send back a response to the person making the request
   res.send({
     success: true,
     message: "Request made successfully",
